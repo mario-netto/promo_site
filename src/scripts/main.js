2489ed3e621970123aeaebab0e6aeccef77a4af8
Mario Netto <marionettojr@gmail.com>
2024-11-09 14:18:29 -0300

    sobre finalizado, add footer, add responsividade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('ok')
+const dataDoEvento = new Date("December 5, 2024 19:00:00");
+const timeStampDoevento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoevento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutosEmMs)
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+    document.getElementById('timer').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras)
+        document.getElementById('timer').innerHTML = 'Evento expirado';
+    }
+}, 1000);
